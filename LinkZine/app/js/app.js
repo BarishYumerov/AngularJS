8e520faa7ccfe2f617e8bff685512380395848fc
BarishYumerov <barish.yumerov@abv.bg>
2015-05-21 19:28:10 +0300

    added login screen

--- LinkZine/app/js/app.js
+++ LinkZine/app/js/app.js
@@ -1,1 +1,25 @@
-var linkZone = angular.module()
+var app = angular.module('app', ['ngRoute', 'ngResource']);
+
+app.constant('baseServiceUrl', 'http://softuni-social-network.azurewebsites.net/api');
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl: 'partials/GuestHome.html',
+        controller: 'GuestHomeController'
+    });
+
+    $routeProvider.when('/register', {
+        templateUrl: 'partials/GuestHome.html',
+        controller: 'GuestHomeController'
+    });
+
+    $routeProvider.otherwise(
+        { redirectTo: '/' }
+    );
+
+});
+
+app.run(function ($rootScope, $location) {
+    $location.path("/");
+});
